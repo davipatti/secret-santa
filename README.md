22ec0011bd073b2ce21bc9f02bcedd1fd5b9c85c
David Pattinson <djp65@cam.ac.uk>
2017-11-12 20:10:32 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Secret Santa
+
+Randomly assign who get's who in Secret Santa.
+
+## Usage
+
+~~~
+secret-santa.py --spec example.json
+~~~
+
+`example.json` contains a dictionary. Keys are the names of everyone involved in the Secret Santa. Values are what's on their wish list. E.g.:
+
+~~~
+{
+    "Alice": "A better influenza vaccine",
+    "Bob": "",
+    "Chris": "World peace"
+}
+~~~
---
+++ secret-santa.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""Randomly assign who gets who in Secret Santa"""
+
+import json
+import random
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Randomly assign who gets who in Secret Santa."
+)
+parser.add_argument(
+    "--spec",
+    dest="spec",
+    help="Specification file.",
+)
+args = parser.parse_args()
+
+with open(args.spec, "r") as handle:
+    spec = json.load(handle)
+
+names = spec.keys()
+
+random.shuffle(names)  # shuffle names inplace
+reordered = list(names)  # copy names
+reordered.insert(0, reordered.pop(-1))  # move last name to first position
+
+for a, b in zip(names, reordered):
+
+    with open("{}.txt".format(a), "w") as fobj:
+        fobj.write("Give a present to: {}".format(b))
+
+        if spec[b] != "":
+            fobj.write("\n\nTheir wish list:\n{}".format(spec[b]))
